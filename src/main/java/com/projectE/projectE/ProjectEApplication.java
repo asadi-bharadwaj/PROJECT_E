1449a0e7bd435dd210befb4e1a3ee66119a532f1
Bharadwaj <bbharadwaj7@gmail.com>
2024-06-29 14:23:20 +0530

    base structure of project_E added

---
+++ src/main/java/com/projectE/projectE/ProjectEApplication.java
@@ -0,0 +1,13 @@
+package com.projectE.projectE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectEApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectEApplication.class, args);
+	}
+
+}
